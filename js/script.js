5baa0e020467410d60ef949434e1fcf8a45490c3
DaneshShahpouri <danesh.shahpouri@gmail.com>
2023-02-23 16:26:58 +0100

    feat: js ticket generator

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
+let userName = document.getElementById("user-name");
 let userDistance = document.getElementById("user-distance");
 let userAge = document.getElementById("user-age");
 let output = document.getElementById("output");
+let outputTime = document.getElementById("output-time");
 
 let btnPrice =  document.getElementById("btn-ticket-price");
 let btnDistance =  document.getElementById("btn-distance");
+let btnTicketGen =  document.getElementById("btn-ticket-generator");
 let ticketPrice;
 let ticketTeen;
 let TicketOld;
 let ticketDiscount;
 
 let travelTime;
+
+let nameOutput = document.getElementById("name-pass");
+let distancePass = document.getElementById("distance-pass");
+let pricePass = document.getElementById("price-pass")
+
+
 
 
 btnPrice.addEventListener("click", function(){
@@ -42,6 +51,7 @@ btnPrice.addEventListener("click", function(){
             ticketPrice = ticketOld;
         }
     
+        ticketPrice;
         output.innerHTML = `Il tuo biglietto costa ${ticketPrice.toFixed(2)} euro`
     };
 
@@ -57,14 +67,28 @@ btnDistance.addEventListener("click", function(){
        if(travelTime>=60){
            travelTime = travelTime/60;
             if(Math.round(travelTime)==1){
-                console.log("circa " + Math.round(travelTime) + " ora")
+                outputTime.innerHTML = "Il tuo viaggio durerà " + "circa " + Math.round(travelTime) + " ora"
             }else{
-                console.log("circa " + Math.round(travelTime) + " ore")
+                outputTime.innerHTML = "Il tuo viaggio durerà " + "circa " + Math.round(travelTime) + " ore"
             }
        } else {
 
-           console.log(parseInt(travelTime) + " minuti")
+           outputTime.innerHTML = "Il tuo viaggio durerà " + parseInt(travelTime) + " minuti"
        }
     }
+})
+
+btnTicketGen.addEventListener("click", function(){
+
+    if(userDistance.value > 0 && userName.value != null && userAge.value > 0 && ticketPrice != undefined){
+        
+        nameOutput.innerHTML = userName.value;
+        distancePass.innerHTML = userDistance.value + " km";
+        pricePass.innerHTML = ticketPrice + " euro";
+
+    } else {
+        output.innerHTML = "Inserisci le informazioni necessarie ed esegui il calcolo del biglietto"
+    }
+
 })
 
